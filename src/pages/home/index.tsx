8205b76fe25513e269acdf9a1adea099f671b58f
larads <dasilval.mariana@gmail.com>
2024-10-11 15:42:39 -0400

    ✨ feat: Visual structure of the Home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import { Heading, Text } from '@ignite-ui/react'
+
+import previewImage from '@/assets/app-preview.png'
+import { Container, Hero, Preview } from "./styles";
+
+export default  function Home() {
+    return(
+        <Container>
+            <Hero>
+                <Heading size="4xl">Agendamento descomplicado</Heading>
+                <Text size="lg">
+                    Conecte seu calendário e permita que as pessoas marquem agendamentos
+                    no seu tempo livre.
+                </Text>
+            </Hero>
+
+            <Preview>
+                <Image src={previewImage} height={400} alt=''/>
+            </Preview>
+        </Container>
+    )
+}
