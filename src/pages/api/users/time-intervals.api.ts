048b73d9b85263286ba10f7115a5066fd8f36b83
larads <dasilval.mariana@gmail.com>
2024-10-13 13:01:26 -0400

    ✨ feat: Persisting intervals

--- src/pages/api/users/time-intervals.api.ts
+++ src/pages/api/users/time-intervals.api.ts
@@ -1,6 +1,19 @@
+import { z } from 'zod'
 import { unstable_getServerSession } from 'next-auth'
 import { NextApiRequest, NextApiResponse } from 'next'
+
+import { prisma } from '../../../lib/prisma'
 import { buildNextAuthOptions } from '../auth/[...nextAuth].api'
+
+const timeIntervalsBodySchema = z.object({
+  intervals: z.array(
+    z.object({
+      weekDay: z.number(),
+      startTimeInMinutes: z.number(),
+      endTimeInMinutes: z.number(),
+    }),
+  ),
+})
 
 export default async function handler(
   req: NextApiRequest,
@@ -9,10 +22,28 @@ export default async function handler(
   if (req.method !== 'POST') {
     return res.status(405).end()
   }
+
   const session = await unstable_getServerSession(
     req,
     res,
     buildNextAuthOptions(req, res),
   )
-  return res.json({ session })
+
+  if (!session) {
+    return res.status(401).end()
+  }
+  const { intervals } = timeIntervalsBodySchema.parse(req.body)
+  await Promise.all(
+    intervals.map((interval) => {
+      return prisma.userTimeInterval.create({
+        data: {
+          week_day: interval.weekDay,
+          time_start_in_minutes: interval.startTimeInMinutes,
+          time_end_in_minutes: interval.endTimeInMinutes,
+          user_id: session.user?.id,
+        },
+      })
+    }),
+  )
+  return res.status(201).end()
 }
